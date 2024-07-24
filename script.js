b7c175f30c603a5a8065afd7511524374f027c7e
sumithranagarajan <yesiamsumithra@gmail.com>
2024-07-24 15:33:00 +0530

    changes commit

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+    document.getElementById("countdown").innerText = "10";
+    setTimeout(() => {
+      document.getElementById("countdown").innerText = "9";
+      setTimeout(() => {
+        document.getElementById("countdown").innerText = "8";
+        setTimeout(() => {
+          document.getElementById("countdown").innerText = "7";
+          setTimeout(() => {
+            document.getElementById("countdown").innerText = "6";
+            setTimeout(() => {
+              document.getElementById("countdown").innerText = "5";
+              setTimeout(() => {
+                document.getElementById("countdown").innerText = "4";
+                setTimeout(() => {
+                  document.getElementById("countdown").innerText = "3";
+                  setTimeout(() => {
+                    document.getElementById("countdown").innerText = "2";
+                    setTimeout(() => {
+                      document.getElementById("countdown").innerText = "1";
+                      setTimeout(() => {
+                        document.getElementById("countdown").innerText =
+                          "Happy Independence Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
